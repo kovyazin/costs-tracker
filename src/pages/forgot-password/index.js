e900a13da13dc1fc293354455620d2c2ba00b352
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-17 17:45:02 +0500

    feat: make forgot password page

--- src/pages/forgot-password/index.js
+++ src/pages/forgot-password/index.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
+import { Form, Input, Button } from 'antd'
+import { MailOutlined } from '@ant-design/icons'
+
+import { AuthCard } from '@features/auth'
+import { ContentCenter } from '@ui'
+
 export const ForgotPasswordPage = () => {
   return (
-    <h1>Forgot Password Page</h1>
+    <ContentCenter>
+      <AuthCard title="Восстановление пароля">
+        <Form size="large">
+          <Form.Item>
+            <Input prefix={<MailOutlined />} placeholder="Введите ваш email" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Сбросить пароль
+            </Button>
+          </Form.Item>
+        </Form>
+      </AuthCard>
+    </ContentCenter>
   )
 }
