2dc4dc4df42cea78597e141f64de4f492239dabb
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-17 13:44:48 +0500

    chore: set aliases

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,19 @@
+const CracoAlias = require('craco-alias')
+
 module.exports = {
-  
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        aliases: {
+          '@': './src',
+          '@features': './src/features',
+          '@lib': './src/lib',
+          '@api': './src/api',
+          '@ui': './src/ui',
+          '@pages': './src/pages'
+        }
+      }
+    }
+  ]
 }
