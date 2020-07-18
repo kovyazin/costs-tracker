9debebf9e8c4a1422dd32717e1210d4c7400b9eb
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-18 13:59:11 +0500

    feat: include firebase

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import { Pages } from '@pages'
+import { FirebaseContext } from '@lib/firebase'
+import { Firebase } from '@api/firebase'
 
 import 'antd/dist/antd.css'
 
 export const App = () => {
   return (
-    <Router>
-      <Pages />
-    </Router>
+    <FirebaseContext.Provider value={new Firebase()}>
+      <Router>
+        <Pages />
+      </Router>
+    </FirebaseContext.Provider>
   )
 }
