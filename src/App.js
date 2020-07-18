09af8a9af26dda13cdd6f8bdcd66fe418ec03698
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-18 16:46:51 +0500

    feat: include redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import { Pages } from '@pages'
 import { FirebaseContext } from '@lib/firebase'
+import { store } from '@lib/store'
 import { Firebase } from '@api/firebase'
 
 import 'antd/dist/antd.css'
@@ -11,9 +13,11 @@ import 'antd/dist/antd.css'
 export const App = () => {
   return (
     <FirebaseContext.Provider value={new Firebase()}>
-      <Router>
-        <Pages />
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Pages />
+        </Router>
+      </Provider>
     </FirebaseContext.Provider>
   )
 }
