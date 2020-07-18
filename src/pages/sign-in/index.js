2c42c678a29bff90d5508609d559cac5f112c766
Slava Kovyazin <slava.kovazin@gmail.com>
2020-07-18 15:57:08 +0500

    feat: implement sign in

--- src/pages/sign-in/index.js
+++ src/pages/sign-in/index.js
@@ -1,15 +1,35 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useContext } from 'react'
 
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, notification } from 'antd'
 import { MailOutlined, LockOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 
 import { ContentCenter } from '@ui'
 import { AuthCard } from '@features/auth'
+import { FirebaseContext } from '@lib/firebase'
 
 import './styles.scss'
 
 export const SignInPage = () => {
+  const [form] = Form.useForm()
+  const firebase = useContext(FirebaseContext)
+  const history = useHistory()
+
+  const handleSubmitForm = async ({ email, password }) => {
+    try {
+      await firebase.doSignInWithEmailAndPassword(email, password)
+
+      form.resetFields()
+
+      history.push('/')
+    } catch (e) {
+      notification.error({
+        message: 'Ошибка авторизации',
+        description: e.message
+      })
+    }
+  }
+
   return (
     <div className="sign-in-page">
       <ContentCenter>
@@ -21,14 +41,41 @@ export const SignInPage = () => {
             </Fragment>
           }
         >
-          <Form size="large">
-            <Form.Item>
+          <Form
+            form={form}
+            onFinish={handleSubmitForm}
+            initialValues={{ email: '', password: '' }}
+            size="large"
+          >
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Пожалуйста, введите ваш email'
+                },
+                {
+                  type: 'email',
+                  message: 'Пожалуйста, введите корректный email адрес'
+                }
+              ]}
+              hasFeedback
+            >
               <Input
                 prefix={<MailOutlined />}
                 placeholder="Введите ваш email"
               />
             </Form.Item>
-            <Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Пожалуйста, введите ваш пароль'
+                }
+              ]}
+              hasFeedback
+            >
               <Input
                 prefix={<LockOutlined />}
                 type="password"
